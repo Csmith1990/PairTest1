50b5c493c7cbecc4f7b49e812dd62aae932d2d5b
CeceAragorn <igkepsi@gmail.com>
2016-09-04 00:52:12 -0300

    initial index js

---
+++ index.js
@@ -0,0 +1,16 @@
+(function ($) {
+    $.fn.invisible = function () {
+        return this.each(function () {
+            $(this).css("visibility", "hidden");
+        });
+    };
+    $.fn.visible = function () {
+        return this.each(function () {
+            $(this).css("visibility", "visible");
+        });
+    };
+}(jQuery));
+
+$(document).ready(function() {
+    
+});
